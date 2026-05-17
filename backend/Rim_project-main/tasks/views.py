cf43ab029e50ad9155c482fde5497318968a5f8a
Maria Neiman <mariaandreevna23042007@gmail.com>
2026-05-17 14:42:30 +0700

    Build: Очистка репозитория от кэша питона и модулей

--- backend/Rim_project-main/tasks/views.py
+++ backend/Rim_project-main/tasks/views.py
@@ -1,33 +1,19 @@
-from rest_framework import viewsets, generics, permissions
-from django.contrib.auth.models import User
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 from .models import Project, Column, Task
-from .serializers import ProjectSerializer, ColumnSerializer, TaskSerializer, RegisterSerializer 
+from .serializers import ProjectSerializer, ColumnSerializer, TaskSerializer
 
 class ProjectViewSet(viewsets.ModelViewSet):
-    serializer_class = ProjectSerializer
-    permission_classes = [permissions.IsAuthenticated]
     queryset = Project.objects.all()
-    def get_queryset(self):
-        return Project.objects.filter(owner=self.request.user)
-
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
+    serializer_class = ProjectSerializer
+    permission_classes = [AllowAny]
 
 class ColumnViewSet(viewsets.ModelViewSet):
-    permission_classes = [permissions.AllowAny]
     queryset = Column.objects.all()
     serializer_class = ColumnSerializer
-    def get_queryset(self):
-        return Column.objects.all()
+    permission_classes = [AllowAny]
 
 class TaskViewSet(viewsets.ModelViewSet):
-    permission_classes =[permissions.AllowAny]
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    def get_queryset(self):
-        return Task.objects.all()
-    
-class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    permission_classes = [permissions.AllowAny]
-    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]

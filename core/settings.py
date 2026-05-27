07632d1b471cdae01a0e96a246d818210809f56a
andazle <andrey.kazakov.200407@mail.ru>
2026-05-27 23:52:49 +0700

    Configure backend for deploy

--- core/settings.py
+++ core/settings.py
@@ -15,7 +15,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env('DEBUG')
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost', '.onrender.com'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost', '.amvera.ru', '0.0.0.0'])
 
 INSTALLED_APPS = [
     "django.contrib.admin",

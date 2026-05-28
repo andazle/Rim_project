b2a815af257e7b026d2e120bba2a2bfc564b2bd4
andazle <andrey.kazakov.200407@mail.ru>
2026-05-28 16:20:07 +0700

    Fix ALLOWED_HOSTS for Render

--- backend/Rim_project-main/core/urls.py
+++ backend/Rim_project-main/core/urls.py
@@ -4,6 +4,12 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions 
+from django.http import HttpResponse  
+
+
+def home(request):
+    return HttpResponse("Привет! Это API проекта Rim Project. Доступные пути: /admin, /api/v1/")
+
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -16,6 +22,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('', home), 
     path('admin/', admin.site.urls),
     path('api/v1/', include('tasks.urls')), 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,12 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from tasks.views import RegisterView
+from django.http import HttpResponse  
+
+
+def home(request):
+    return HttpResponse("Привет! Это API проекта Rim Project. Доступные пути: /admin, /api/v1/")
+
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -17,6 +23,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('', home), 
     path('admin/', admin.site.urls),
     path('api/v1/', include('tasks.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),

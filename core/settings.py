27b9368d1eb463ea174a97879be4b5aed99445e8
andazle <andrey.kazakov.200407@mail.ru>
2026-05-28 16:13:08 +0700

    Fix ALLOWED_HOSTS for Render

--- core/settings.py
+++ core/settings.py
@@ -15,7 +15,12 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env('DEBUG')
-ALLOWED_HOSTS = ['scheduler-backend-x9ec.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'rim-project.onrender.com',           
+    'scheduler-backend-x9ec.onrender.com', 
+    'localhost',
+    '127.0.0.1'
+]
 
 
 INSTALLED_APPS = [

e4445b4c9c092045198f5cf8e06808597018c4ac
andazle <andrey.kazakov.200407@mail.ru>
2026-05-28 17:28:58 +0700

    Fix ALLOWED_HOSTS for Render

--- core/settings.py
+++ core/settings.py
@@ -125,7 +125,7 @@ SWAGGER_SETTINGS = {
 }
 
 CORS_ALLOWED_ORIGINS = [
-    "https://andazle.github.io",
+    "https://rimproject.netlify.app/",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]

eb0ef85ea7497c9ee14442757fca478a4b37358a
andazle <andrey.kazakov.200407@mail.ru>
2026-06-02 19:56:07 +0700

    оптимизация

--- backend/tasks/serializers.py
+++ backend/tasks/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import Project, Column, Task
 from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
